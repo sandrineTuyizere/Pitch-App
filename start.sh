411bd5926ab226888ead96f57271fdb8cae7f9be
Sandrine Tuyizere <sandrauyizere@gmail.com>
2020-12-07 20:51:49 +0200

    first commit

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY=002
+export DATABASE_URL=postgresql+psycopg2://sandrine:7676@localhost/blogs
+export MAIL_USERNAME=sandrauyizere@gmail.com
+export MAIL_PASSWORD=ubumuntu
+python3.6 manage.py run
