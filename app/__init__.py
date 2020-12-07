07317ecc4d43cd0b69feeb4abbc68c80fc50718f
Sandrine Tuyizere <sandrauyizere@gmail.com>
2020-12-07 21:09:52 +0200

    modifying app

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import config_options
 from flask_mail import Mail
 from flask_login import LoginManager
-# from flask_bootstarp import Bootstarp
+
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import IMAGES,UploadSet,configure_uploads
